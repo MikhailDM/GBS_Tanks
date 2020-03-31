936d5101f702d2c246dce2015673234945d033f8
MikeDmitriev <misha.dmitriev.spb@gmail.com>
2020-03-31 21:49:10 +0300

    2 Тестовых ECS проекта

---
+++ Assets/ECS_Test1/Systems/Models/HealthSystem.cs
@@ -0,0 +1,40 @@
+﻿using Morpeh;
+using UnityEngine;
+using Unity.IL2CPP.CompilerServices;
+
+[Il2CppSetOption(Option.NullChecks, false)]
+[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+[Il2CppSetOption(Option.DivideByZeroChecks, false)]
+[CreateAssetMenu(menuName = "ECS/Systems/" + nameof(HealthSystem))]
+public sealed class HealthSystem : UpdateSystem {
+
+    private Filter filter;
+
+    public override void OnAwake() {
+        //Ищем через фильтр компонент
+        this.filter = this.World.Filter.With<HealthComponent>();
+        //You can chain filters by two operators With<> and Without<>.
+        //For example this.World.Filter.With<FooComponent>().With<BarComponent>().Without<BeeComponent>();
+    }    
+
+    public override void OnUpdate(float deltaTime)
+    {
+        var healthBag = this.filter.Select<HealthComponent>();
+        //Перебираем все сущности в фильтре
+        for (int i = 0, length = this.filter.Length; i < length; i++)
+        {
+            ref var healthComponent = ref healthBag.GetComponent(i);
+            //Печатает количество жизней у сущности
+            Debug.Log("Health Points is:" + healthComponent.healthPoints);
+            Debug.Log("Health Text is:" + healthComponent.healthText);
+        }
+
+        //Перебираем все сущности в фильтре
+        /*foreach (var entity in this.filter)
+        {
+            ref var healthComponent = ref entity.GetComponent<HealthComponent>();
+            //Печатает количество жизней у сущности
+            Debug.Log(healthComponent.healthPoints);
+        }*/
+    }
+}
---
+++ Assets/ECS_Test2/Systems/ExamplePositionSystem.cs
@@ -0,0 +1,33 @@
+﻿using Morpeh;
+using UnityEngine;
+using Unity.IL2CPP.CompilerServices;
+
+[Il2CppSetOption(Option.NullChecks, false)]
+[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+[Il2CppSetOption(Option.DivideByZeroChecks, false)]
+[CreateAssetMenu(menuName = "ECS/Systems/" + nameof(ExamplePositionSystem))]
+public sealed class ExamplePositionSystem : UpdateSystem {
+    private Filter filter;
+
+
+    public override void OnAwake() {
+        this.filter = this.World.Filter.With<UnitComponent>();
+
+        //Создаем обьекты
+        for (int i = 0, length = 5; i < length; i++)
+        {
+           var ent = World.CreateEntity();
+           ent.AddComponent<UnitComponent>();
+        }
+    }
+
+    public override void OnUpdate(float deltaTime) {
+        foreach (var entity in this.filter)
+        {
+            ref var unit = ref entity.GetComponent<UnitComponent>();
+            //Перемещает сущности
+            unit.Position = unit.Position + Vector3.one * deltaTime;
+            Debug.Log("Test");
+        }
+    }
+}
